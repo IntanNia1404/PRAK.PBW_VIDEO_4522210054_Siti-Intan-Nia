3ebebbe54243139ef24600c36a896f30829102b6
IntanNia1404 <intannia1411@gmail.com>
2024-11-21 23:03:15 -0500

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<x-app-layout>
+<x-slot name="heading">About</x-slot>
+    About Content
+</x-app-layout>
---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+<x-slot name="heading">Galerry</x-slot>
+Gallery Content
+   
+</x-app-layout>
