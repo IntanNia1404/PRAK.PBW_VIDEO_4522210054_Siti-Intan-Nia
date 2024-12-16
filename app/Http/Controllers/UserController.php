a03a606d3340d472f01133ff9daf891c4a97db7a
IntanNia1404 <intannia1411@gmail.com>
2024-12-16 08:04:59 -0500

    video 10

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -19,5 +19,12 @@ class UserController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+       User::create($request->only('name','email','password'));
+
+       return redirect('/users');
     }
 }
--- resources/views/Users/create.blade.php
+++ resources/views/Users/create.blade.php
@@ -1,4 +1,22 @@
 <x-app-layout title="Create Users">
 <x-slot name="heading">Create Users</x-slot>
-    Create Users
+    <form action="/users" method="post" class="space-y-6">
+    @csrf
+    <div>
+        <label for="name">Name</label>
+        <input class="border px-4 py-2 rouded block mt-1" type="text" name="name" id="name">
+    </div>
+    <div>
+        <label for="name">Email</label>
+        <input class="border px-4 py-2 rouded block mt-1" type="email" name="email" id="email">
+    </div>
+    <div>
+        <label for="password">password</label>
+        <input class="border px-4 py-2 rouded block mt-1" type="password" name="password" id="password">
+    </div>
+
+    <x-button>
+        Save
+    </x-button>
+    </form>
 </x-app-layout>
