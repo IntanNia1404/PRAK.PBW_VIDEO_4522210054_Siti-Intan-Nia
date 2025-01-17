d88f2875c4a2fabe3274047611f43ddf7673bb8c
IntanNia1404 <intannia1411@gmail.com>
2025-01-17 12:28:36 -0500

    video 13- 15

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use \App\Models\User;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 class UserController extends Controller
 {
@@ -11,30 +12,53 @@ class UserController extends Controller
        $users = User::query()->get();
 
        return view('users.index', [
-             'users'=>$users,
+        'users'=>$users,
        ]);
     }
     public function create()
     {
-        return view('users.create');
+        return view('users.form', [
+            'user' => new User(),
+            'page_meta' => [
+                'title' => 'Create new user',
+                'method' => 'post',
+                'url' => '/users',
+                'submit_text' => 'Create'
+            ],
+        ]);
     }
 
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-      User::create( $request->validate([
-        'name' => ['required', 'min:3', 'max:255', 'string'],
-        'email' => ['required', 'email'],
-        'password' => ['required', 'min:8'],
-      ]));
+    User::create( $request->validated());
 
         return redirect('/users');
     }
 
     public function show(user $user)
-   
     {
         return view('users/show', [
             'user' => $user,
         ]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.form', [
+            'user' => $user,
+            'page_meta' => [
+                'title' => 'Edit user: ' . $user->name,
+                'method' => 'put',
+                'url' => '/users/' . $user->id,
+                'submit_text' => 'Update'
+            ],
+        ]);
+    }
+
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update( $request->validated());
+    
+        return redirect('/users');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,18 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
-use App\Models\Article;
+use App\Http\AboutController;
+
 Route::get('/', [Controllers\HomeController::class, 'index']);
 Route::get('/about', [Controllers\AboutController::class, 'index']);
 Route::get('/contact', [Controllers\ContactController::class, 'index']);
 Route::get('/gallery', [Controllers\GalleryController::class, 'index']);
+
 Route::get('/users', [Controllers\UserController::class, 'index']);
 Route::get('/users/create', [Controllers\UserController::class, 'create']);
 Route::post('/users', [Controllers\UserController::class, 'store']);
-Route::get('/users/{user:id}',[Controllers\UserController::class,'show']);
+Route::get('/users/{user:id}', [Controllers\UserController::class, 'show']);
+Route::get('/users/{user:id}/edit', [Controllers\UserController::class, 'edit']);
+Route::put('/users/{user:id}', [Controllers\UserController::class, 'update']);
